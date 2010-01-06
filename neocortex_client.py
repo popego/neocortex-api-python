f12ee78d2f5f8fa7e64d6b9a677b846e027ed310
vic <victor@popego.com>
2010-01-06 15:17:05 -0300

    alpha client

---
+++ neocortex_client.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+__docformat__="restructuredtext"
+
+from utils.rest import RestClient, Result, ResponseFormats
+from datetime import datetime
+   
+
+class NeocortexRestClient(object):
+    BASE_URL = "http://brain.meaningtool.com/neocortex"
+    __builder__ = None
+    
+    class Builder(RestClient):
+        _functions = {}
+        _params = {}
+        _input = None
+        _format = None
+        _tree_key = None
+                
+        def format(self, value):
+            self._format = value
+            return self
+        
+        def input(self, text):
+            self._input = self._params["input"] = text
+            return self
+        
+        def categories(self, tree_key=None, additionals=None):
+            self._functions["categories"] = dict(tree_key=tree_key or self._tree_key, additionals=additionals or [])
+            return self
+        
+        def keywords(self):
+            self._functions["keywords"] = True
+            return self
+        
+        def entities(self):
+            self._functions["entities"] = True
+            return self
+        
+        def language(self):
+            self._functions["language"] = True
+            return self
+        
+        def meaningfy(self):
+            fs = []
+            for k,v in self._functions.items():
+                kk = k
+                if isinstance(v, dict):
+                    if v.has_key("additionals"):
+                        for a in v["additionals"]:
+                            kk = "%s+%s" % (kk, a)
+                    if v.has_key("tree_key"):
+                        self._params["tree_key"] = v["tree_key"] 
+                fs.append(kk)
+            fs = ";".join(fs)
+            url = "%s.%s" % (fs, self._format)
+            res = self.post(url, self._params, response_format=self._format)
+            self._reset()    
+            return res
+
+        def _reset(self):
+            self._functions = {}
+            self._params = {}
+    
+
+    def __init__(self, api_key, base_url=None):
+        self.api_key = api_key
+        self.BASE_URL = base_url or self.BASE_URL
+
+    def get_builder(self):
+        if self.__builder__ is None:
+            self.__builder__ = NeocortexRestClient.Builder(self.BASE_URL)
+        return self.__builder__
+
+    def categories(self, input, tree_key=None, additionals=None):
+        builder = self.get_builder()
+        return builder.format(ResponseFormats.JSON).input(input).categories(tree_key, additionals).meaningfy().payload["categories"]
+
+    def keywords(self, input):
+        builder = self.get_builder()
+        return builder.format(ResponseFormats.JSON).input(input).keywords().meaningfy().payload["keywords"]
+        
+    def entities(self, input):
+        builder = self.get_builder()
+        return builder.format(ResponseFormats.JSON).input(input).entities().meaningfy().payload["entities"]
+
+    def language(self, input):
+        builder = self.get_builder()
+        return builder.format(ResponseFormats.JSON).input(input).language().meaningfy().payload["language"]
---
+++ rest.py
@@ -0,0 +1,235 @@
+import re
+import urllib
+import urllib2
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+
+_re_url = re.compile(ur"^https?://.+$")
+
+
+###############################################################################
+## Exceptions
+###############################################################################
+
+class BaseRestClientError(Exception):
+    message = u"There was an error while getting the data."
+
+class UnknownException(BaseRestClientError):
+    message = u"There was an unknown error while getting the data."
+
+class ResponseBodyFormatNotValid(BaseRestClientError):
+    message = u"The response body is not in the expected format."
+    
+class InvalidParameter(BaseRestClientError):
+    """ 
+    Exception raised when a parameter of the client had an invalid value.
+    """
+    message = u"A parameter has an invalid value."
+
+class InvalidUrl(InvalidParameter):
+    """ 
+    Exception raises when a url is invalid. This can be happen
+    on two situations:
+        - the input is an url.
+        - the `url_hint` parameter was used.
+    """
+    message = u"The url is invalid."
+
+
+
+
+###############################################################################
+## Response formats, parsers and Result classes
+###############################################################################
+
+class Result(object):
+    """
+    A unified access to the retrieved API results
+    """
+    def __init__(self, status_code, status_message, payload=None, metadata=None):
+        """
+        Creates the result.
+        
+        :parameters:
+            status_code: str
+                the status of the response.
+            status_message: str
+                the message of the response.
+            payload: dict()
+                the payload data of the response.
+        """
+        self.code = status_code
+        self.message = status_message
+        self.payload = payload
+        self.metadata = metadata or {}
+        
+    def __repr__(self):
+        return u"<%s - %s>" % (self.__class__.__name__, self.message)
+    
+    
+class ResponseParser(object):
+    """
+    Responsible for parse the raw API response
+    """
+    result_class = None
+    available_exceptions = None
+    
+    def __init__(self, result_class=None, available_exceptions=None):
+        self.result_class = result_class or self.result_class or Result
+        self.available_exceptions = available_exceptions or self.available_exceptions or []
+    
+    def parse(self, raw):
+        raise NotImplementedError("'parse' method must be implemented.")
+    
+    def _parse_dict(self, rdict):
+        status = rdict["status"]
+        message = rdict["message"]
+        code = rdict["code"]
+        payload = rdict.get("payload", None)
+        metadata = rdict.get("metadata", None)
+        
+        if status == "ok":
+            if self.result_class is not None:
+                return self.result_class(code, message, payload, metadata)
+            else:
+                return (code, message, payload, metadata)
+        else:
+            self._raise_exception(code)
+            
+    def _raise_exception(self, code):
+        if self.available_exceptions is None:
+            raise UnknownException()
+        
+        for exception in self.available_exceptions:
+            if (hasattr(exception, "code") and code == exception.code) \
+                or (hasattr(exception, "get_code") and code == exception.get_code()) \
+                or exception.__name__ == code:
+                raise exception()
+            
+        raise UnknownException()
+
+class RawResponseParser(ResponseParser):
+    def parse(self, raw):
+        return raw
+
+class XmlResponseParser(ResponseParser):
+    def parse(self, raw):
+        return raw
+    
+class JsonResponseParser(ResponseParser):
+    def parse(self, raw):
+        try:
+            d = json.loads(raw, encoding="utf8")
+        except:
+            raise ResponseBodyFormatNotValid()
+        return self._parse_dict(d)
+
+class ResponseFormats(object):
+    XML = "xml"
+    JAVASCRIPT = "js"
+    JSON = "json"
+
+
+
+###############################################################################
+## Base Rest Client
+###############################################################################
+
+class RestClient(object):
+    
+    POSIBLE_HTTP_CODES = [400, 401, 403, 404, 409, 500]
+    
+    base_url = None
+    api_key = None
+    result_class = None
+    available_exceptions = None
+    parsers_map = None
+    
+    _response_format = ResponseFormats.JSON
+    
+
+    def __init__(self, base_url=None, api_key=None, result_class=None, available_exceptions=None, parsers_map=None,):
+        self.base_url = base_url or self.base_url
+        self.api_key = api_key or self.api_key
+        if parsers_map is None and self.parsers_map is None:
+            self.result_class = result_class or self.result_class or Result
+            self.available_exceptions = available_exceptions or self.available_exceptions
+            self.parsers_map = {
+                "xml":XmlResponseParser(self.result_class, self.available_exceptions),
+                "js": RawResponseParser(self.result_class, self.available_exceptions),
+                "json": JsonResponseParser(self.result_class, self.available_exceptions)
+            }
+        else:
+            self.parsers_map = parsers_map
+
+    def get(self, url, data=None, headers=None, response_format=ResponseFormats.JSON):
+        url = self._get_full_url(url)
+        self._response_format = response_format
+        return self._req("GET", url, data, headers)
+
+    def post(self, url, data=None, headers=None, response_format=ResponseFormats.JSON):
+        url = self._get_full_url(url)
+        self._response_format = response_format
+        return self._req("POST", url, data, headers)
+
+    def put(self, url, data=None, headers=None, response_format=ResponseFormats.JSON):
+        url = self._get_full_url(url)
+        data = data or {}
+        data['_method'] = 'PUT'
+        self._response_format = response_format
+        return self._req("POST", url, data, headers)
+
+    def delete(self, url, data=None, headers=None, response_format=ResponseFormats.JSON):
+        url = self._get_full_url(url)
+        data = data or {}
+        data['_method'] = 'DELETE'
+        self._response_format = response_format
+        return self._req("POST", url, data, headers)
+
+    def _req(self, method, url, data=None, headers=None):
+        headers = headers or []
+        
+        # validate url
+        if not _re_url.match(url):
+            raise InvalidUrl(url)
+        
+        # validate method
+        if method == "GET":
+            req = urllib2.Request(u"%s?%s" % (url, urllib.urlencode(data)))
+        elif method == "POST":
+            req = urllib2.Request(url, urllib.urlencode(data))
+        else:
+            raise ValueError(u"HTTP Method '%s' not supported" % method)
+        
+        if self._response_format == ResponseFormats.JSON:
+            req.add_header("Accept", "application/json")
+        elif self._response_format == ResponseFormats.JAVASCRIPT:
+            req.add_header("Accept", "application/javascript")
+        elif self._response_format == ResponseFormats.XML:
+            req.add_header("Accept", "application/xml")
+            
+        req.add_header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+        req.add_header("Accept-Charset", "UTF-8")
+        for k,v in headers:
+            req.add_header(k, v)
+        
+        try:
+            resp = urllib2.urlopen(req)
+        except urllib2.HTTPError, e:
+            if e.code not in self.POSIBLE_HTTP_CODES:
+                raise BaseRestClientError("There was an error while getting the data.")
+            resp = e
+        
+        parser = self.parsers_map.get(self._response_format, RawResponseParser())
+        return parser.parse(resp.read())
+    
+    def _get_full_url(self, url):
+        if not self.base_url.endswith("/"):
+            self.base_url = "%s/" % self.base_url
+        if url.startswith("/"):
+            url = url[1:]
+        return "%s%s" % (self.base_url, url)
